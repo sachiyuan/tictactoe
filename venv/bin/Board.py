e7884b7c1d17c45d84b3f203edbd0af39340dca8
Sachi <SachiYuan@gmail.com>
2020-07-03 22:25:07 -0700

    2 Player version semi working
    Detects wins in rows or columns or diagonals, but ends game prematurely if any row/column/diagonal is filled even if there are other spaces.

--- venv/bin/Board.py
+++ venv/bin/Board.py
@@ -6,34 +6,44 @@ class Board:
     def __init__(self):
         self.layout = numpy.zeros((3, 3))
 
+    def insert(self, place, sym):
+        symtoval = {'X': 1, 'O': 2}
+        if self.layout[place[0], place[1]] == 0:
+            self.layout[place[0], place[1]] = symtoval[sym]
+            return True
+        else:
+            print("That space is already taken.")
+            return False
+
     def check_end(self, last_move):
         """
         Check if the game is finished
         :param last_move: Coordinates of the last move (x,y)
         :return result:  True if the game is not over, False if it is
         """
-        x = last_move[0]
-        y = last_move[1]
+        x = last_move[1]
+        y = last_move[0]
         col_result = self.check_column(x)
         row_result = self.check_row(y)
-        if col_result + row_result != 0:
-            if col_result == 1 or row_result == 1:
+        diag_result = self.check_diagonal((y,x))
+        if col_result + row_result + diag_result != 0:
+            if col_result == 1 or row_result == 1 or diag_result == 1:
                 print("X wins!")
-            elif col_result == 2 or row_result == 2:
+            elif col_result == 2 or row_result == 2 or diag_result ==2:
                 print("O wins!")
             else:
-                print("Tie!")
+                print("Tie!!")
             return True
         else:
             return False
-
+#TODO: Make it so it doesn't end game when column is full if board is not full
     def check_column(self, x):
         """
         Check if column is full and if there is a victor
         :param x: Which column
         :return: return 0 if not full, 1 if X is victor, 2 if O is victor, and 3 if tied
         """
-        same = np.all(self.layout == self.layout[0, :], axis=0)[x]  # returns whether all values in column x are same
+        same = numpy.all(self.layout == self.layout[0, :], axis=0)[x]  # returns whether all values in column x are same
         if same:
             return self.layout[0][x]  # if filled with 0, returns 0, likewise returns corresponding val for X and O
         else:
@@ -57,20 +67,44 @@ class Board:
         else:
             return 0
 
+    def check_diagonal(self, place):
+        """
+        Check if the diagonal is full and if there is a victor
+        :param place: coordinates of space
+        :return: 0 if not full, 1 if X is victor, 2 if 0 is victor, and 3 if tied
+        """
+        if place[0] == place[1]: #aka if place in {(0,0), (1,1), (2,2)}
+            #testing the top left to bottom right diagonal
+            diagonal = self.layout.diagonal()
+            if 0 not in diagonal:
+                if numpy.all(diagonal == diagonal[0]):
+                    return diagonal[0]
+                else:
+                    return 3
+            else:
+                return 0
+        if place in {(0, 2), (1,1), (2,0)}:
+            first = self.layout[0,2]
+            second = self.layout[1,1]
+            third = self.layout[2,0]
+            if first == second == third:
+                return first
+            else:
+                if 0 in {first, second, third}:
+                    return 0
+                else:
+                    return 3
+
     def print_board(self):
-        valtosym = {0: '_', 1: 'X', 2: 'O'}
-        board = "    |     |    \n" \
-                    "  "+{}+"  |  "+{}+"  |  "+{}+ "  \n" +\
-                    "____|____|____"+ \
-                    "    |     |    \n" \
-                    "  " + {} + "  |  " + {} + "  |  " + {} + "  \n" + \
-                    "____|____|____" + \
-                    "    |     |    \n" \
-                    "  " + {} + "  |  " + {} + "  |  " + {} + "  \n" + \
-                    "     |     |     ".format(valtosym[self.layout[0, 0]], valtosym[self.layout[0, 1]],
-                                               valtosym[self.layout[0, 2]], valtosym[self.layout[1, 0]],
-                                               valtosym[self.layout[1, 1]], valtosym[self.layout[1, 2]],
-                                               valtosym[self.layout[2, 0]], valtosym[self.layout[2, 1]],
-                                               valtosym[self.layout[2, 2]])
+        valtosym = {0: ' ', 1: 'X', 2: 'O'}
+        board = "\n  {}  |  {}  |  {}   \n".format(valtosym[self.layout[0, 0]], valtosym[self.layout[0, 1]],
+                                               valtosym[self.layout[0, 2]]) +\
+                    "_____|_____|_____\n"+ \
+                    "  {}  |  {}  |  {}  \n".format(valtosym[self.layout[1, 0]],
+                                               valtosym[self.layout[1, 1]], valtosym[self.layout[1, 2]]) + \
+                    "_____|_____|_____\n" + \
+                    "  {}  |  {}  |  {}  \n".format( valtosym[self.layout[2, 0]], valtosym[self.layout[2, 1]],
+                                               valtosym[self.layout[2, 2]]) + \
+                    "     |     |      "
         print(board)
 
--- venv/bin/Game.py
+++ venv/bin/Game.py
@@ -1,9 +1,9 @@
-from Board.py import Board
+from Board import Board
 
 
 def game():
     single_player = False
-    players = input("Please input 1 or 2 players.")
+    players = input("Please input 1 or 2 players.\n")
     if players == "1":
         print("You have selected single-player mode.")
         single_player = True
@@ -11,20 +11,57 @@ def game():
         print("That was not one of the options. You have been defaulted to 2 player mode.")
     else:
         print("You have selected 2 player mode.")
-
+    playing = True
     board = Board()
-    example_board = "    |     |    \n" \
-                    "  "+{}+"  |  "+{}+"  |  "+{}+ "  \n" +\
-                    "____|____|____"+ \
-                    "    |     |    \n" \
-                    "  " + {} + "  |  " + {} + "  |  " + {} + "  \n" + \
-                    "____|____|____" + \
-                    "    |     |    \n" \
-                    "  " + {} + "  |  " + {} + "  |  " + {} + "  \n" + \
-                    "     |     |     ".format('1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+    example_board = "\n  {}  |  {}  |  {}   \n".format('1', '2', '3') +\
+                    "_____|_____|_____\n"+ \
+                    "  {}  |  {}  |  {}  \n".format('4', '5', '6', ) + \
+                    "_____|_____|_____\n" + \
+                    "  {}  |  {}  |  {}  \n".format('7', '8', '9') + \
+                    "     |     |      "
     print("When it is your turn, indicate which space you would like to use by inputting the number of that space")
     print(example_board)
     print("You may not use a space which has already been filled")
     print("The game will end when one player has filled a row or column with their symbol, or when the board is filled")
+    x_turn = True
     if not single_player:
         print("X goes first")
+
+
+
+        while True:
+            board.print_board()
+            symbol = "O"
+            success = False
+            if x_turn:
+                symbol = "X"
+            place = input("Where would you like to go?\n")
+            try:
+                place = int(place)
+                if 0 < place < 10:
+                    place -= 1
+                    y = place // 3
+                    x = place % 3
+                    success = board.insert((y, x), symbol)
+                else:
+                    print("Please input a number on the board")
+                if not success:
+                    print("Please input a space that has not been taken.")
+                else:
+                    x_turn = not x_turn
+                    if board.check_end((y, x)):
+                        board.print_board()
+                        break
+            except:
+                print("Not a valid input. Please try again.")
+
+
+
+
+
+
+
+
+
+game()
